c9d6f8ba80418f83e7745280d8ab8f40ce7e0525
matheusmg123 <50761909+matheusmg123@users.noreply.github.com>
2019-05-27 17:37:17 -0300

    V-alpha 0

---
+++ Game Project/Assets/Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+
+    }
+}
---
+++ Game Project/Assets/Scripts/Core/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Horror.Core
+{
+    public class Movement : MonoBehaviour
+    {
+        public float movementSpeed;
+
+        void FixedUpdate()
+        {
+            if (Input.GetKey("w") && !Input.GetKey("s"))
+            {
+                transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey("s") && !Input.GetKey("w"))
+            {
+                transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+            }
+            if (Input.GetKey("a") && !Input.GetKey("d"))
+            {
+                transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey("d") && !Input.GetKey("a"))
+            {
+                transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+            }
+        }
+
+    }
+}
